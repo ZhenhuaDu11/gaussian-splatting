624ddb3fcb74cb7121ddd4fefe304fdb3b552498
Du_NUDT <meterodu@163.com>
2024-05-30 22:33:03 +0800

    更新子项目 SIBR_viewers 和 simple-knn，增加训练、评估脚本gs.sh

---
+++ gs.sh
@@ -0,0 +1,20 @@
+# Dataset
+# ./Dataset/
+# deepblending/drjohnson, playroom
+# tanksandtemples/train, truck
+# Mip-NeRF360/bicycle, bonsai, counter, flowers, garden, kitchen, room, stump, treehill
+
+# Output
+# ../Output/
+
+# Train
+python train.py -s <Dataset> -m <Output> 
+
+# Eval
+python train.py -s <Dataset> -m <Output> --eval
+python render.py -s <Dataset> -m <Output>
+python metrics.py -s <Dataset> -m <Output>
+
+# SIBR Viewer
+./SIBR_viewers/install/bin/SIBR_remoteGaussian_app -m <Output>
+./SIBR_viewers/install/bin/SIBR_remoteGaussian_app -m <Output>
