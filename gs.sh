00645e8a7ac70a48e2129c588de7998782d4609b
Du_NUDT <meterodu@163.com>
2024-08-22 13:35:44 +0800

    update 08/22

--- gs.sh
+++ gs.sh
@@ -1,5 +1,5 @@
 # Dataset
-# ./Dataset/
+# ../Dataset/
 # deepblending/drjohnson, playroom
 # tanksandtemples/train, truck
 # Mip-NeRF360/bicycle, bonsai, counter, flowers, garden, kitchen, room, stump, treehill
@@ -17,4 +17,4 @@ python metrics.py -s <Dataset> -m <Output>
 
 # SIBR Viewer
 ./SIBR_viewers/install/bin/SIBR_remoteGaussian_app -m <Output>
-./SIBR_viewers/install/bin/SIBR_remoteGaussian_app -m <Output>
+./SIBR_viewers/install/bin/SIBR_gaussianViewer_app -m <Output>
